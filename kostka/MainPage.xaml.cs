00c5fe9907b53a9ee63ff6fd2ca8477e359d58d7
Oskar Malinowski <oszezol@gmail.com>
2024-09-16 13:01:17 +0200

    pkt3

--- kostka/MainPage.xaml.cs
+++ kostka/MainPage.xaml.cs
@@ -2,7 +2,7 @@
 {
     public partial class MainPage : ContentPage
     {
- 
+
 
         public MainPage()
         {
@@ -17,7 +17,8 @@
             {
                 diceRoll = r.Next(1, 5);
             }
-            if (K6.IsChecked) {
+            if (K6.IsChecked)
+            {
                 diceRoll = r.Next(1, 7);
             }
             if (K10.IsChecked)
@@ -29,8 +30,17 @@
                 diceRoll = r.Next(1, 13);
             }
             RollLabel.Text = "Wynik rzutu: " + diceRoll.ToString();
-        
-        }
-    }
 
+        }
+
+        private void CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            RadioButton rb = (RadioButton)sender;
+            int maxRoll = int.Parse((string)rb.Value);
+            string fileName = "k" + maxRoll + ".jpg";
+            DiceImage.Source = fileName;
+            RollLabel.Text = string.Empty;
+        }
+
+    }
 }
